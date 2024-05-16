c89787f9c862c1eea35d7e81a3f4564d22aa0c7c
robin croft <rob@robincroft.com>
2024-05-16 17:40:16 +0100

    get object

--- src/s3Manager.ts
+++ src/s3Manager.ts
@@ -1,5 +1,7 @@
 // @ts-nocheck
-import { S3Client, PutObjectCommand, GetObjectCommand, getSignedUrl } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const logger = require('./logger');
 
