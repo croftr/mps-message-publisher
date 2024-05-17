ea292aefc4d5d21ae5c073b2759faacdaf442eb7
robin croft <rob@robincroft.com>
2024-05-17 15:42:47 +0100

    shutdown

--- index.ts
+++ index.ts
@@ -177,13 +177,14 @@ const go = async () => {
     console.error("Error", error);
 
   } finally {
-    exec('shutdown -h now', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing shutdown: ${error}`);
-        return;
-      }
-      console.log(`Shutdown command executed successfully.`);
-    });    
+    // exec('shutdown -h now', (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`Error executing shutdown: ${error}`);
+    //     return;
+    //   }
+    //   console.log(`Shutdown command executed successfully.`);
+    // });    
+    logger.info("finally section")
   }
 
 }
